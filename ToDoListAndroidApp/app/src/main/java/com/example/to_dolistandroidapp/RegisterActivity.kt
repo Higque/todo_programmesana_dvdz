fab39a8f7d3069f08523d1e1eecd77e429c60a66
zuba5tik <thezuba5tik@gmail.com>
2021-03-18 02:41:01 +0200

    Add validation for registration fields

--- ToDoListAndroidApp/app/src/main/java/com/example/to_dolistandroidapp/RegisterActivity.kt
+++ ToDoListAndroidApp/app/src/main/java/com/example/to_dolistandroidapp/RegisterActivity.kt
@@ -6,6 +6,8 @@ import android.content.Intent
 import kotlinx.android.synthetic.main.activity_register.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import android.widget.Toast
 import retrofit2.Call
 import retrofit2.Callback
@@ -27,11 +29,25 @@ class RegisterActivity : AppCompatActivity() {
         var API = rf.create(RetrofitInterface::class.java)
 
         btnSignUp.setOnClickListener {
-            var encodedPassword = Base64.getEncoder().encodeToString(passwordSignUp.text.toString().toByteArray())
-            var encodedEmail = Base64.getEncoder().encodeToString(emailSignUp.text.toString().toByteArray())
-            signUp(userNameSignUp.text.toString(), encodedEmail, encodedPassword, API)
+            if (userNameSignUp.text.isEmpty()) {
+                userNameSignUp.error = "Fill User Name field"
+            }
+            if (emailSignUp.text.isEmpty()){
+                emailSignUp.error = "Fill Email field"
+            } else if (!isValidEmail(emailSignUp.text.toString())) {
+                emailSignUp.error = "Invalid email"
+            }
+            if (passwordSignUp.text.isEmpty()) {
+                passwordSignUp.error = "Fill Password field"
+            } else if (!IsValidPassword(passwordSignUp.text.toString())) {
+                passwordSignUp.error = "Password must be 8 or more characters in length and must contain 1 or more digit character"
+            }
+            if (userNameSignUp.error.isNullOrEmpty() && emailSignUp.error.isNullOrEmpty() && passwordSignUp.error.isNullOrEmpty()) {
+                var encodedPassword = Base64.getEncoder().encodeToString(passwordSignUp.text.toString().toByteArray())
+                var encodedEmail = Base64.getEncoder().encodeToString(emailSignUp.text.toString().toByteArray())
+                signUp(userNameSignUp.text.toString(), encodedEmail, encodedPassword, API)
+            }
         }
-
     }
 
     fun signUp(userName: String, email: String, password: String, API: RetrofitInterface) {
@@ -59,5 +75,12 @@ class RegisterActivity : AppCompatActivity() {
             }
 
         })
+    }
+    fun isValidEmail(email: String): Boolean {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+    fun  IsValidPassword(password: String): Boolean {
+        val regex = Regex("(?=.*[0-9]).{8,}$", RegexOption.IGNORE_CASE)
+        return regex.matches(password)
     }
 }
