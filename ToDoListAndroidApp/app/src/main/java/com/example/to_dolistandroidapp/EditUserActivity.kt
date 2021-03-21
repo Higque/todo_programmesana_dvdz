7e2c8eb943afcd85ac448a19cad1d73e896071bc
zuba5tik <thezuba5tik@gmail.com>
2021-03-21 21:54:13 +0200

    Modify validation for UserName to be 5 or more characters in length

--- ToDoListAndroidApp/app/src/main/java/com/example/to_dolistandroidapp/EditUserActivity.kt
+++ ToDoListAndroidApp/app/src/main/java/com/example/to_dolistandroidapp/EditUserActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_edit_user.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
@@ -17,11 +18,11 @@ class EditUserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_user)
 
-        var rf = Retrofit.Builder()
+        val rf = Retrofit.Builder()
             .baseUrl(RetrofitInterface.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create()).build()
 
-        var API = rf.create(RetrofitInterface::class.java)
+        val API = rf.create(RetrofitInterface::class.java)
 
         val loggedUserId = intent.getStringExtra("loggedUserId")
         val loggedUserName = intent.getStringExtra("loggedUserName")
@@ -33,51 +34,55 @@ class EditUserActivity : AppCompatActivity() {
 
         btnEditUserName.setOnClickListener {
             if (editUserName.text.toString() != "") {
-                if (loggedUserId != null && loggedEmail != null && loggedPassword != null) {
-                    var call =
-                        API.putUser(
-                            id = loggedUserId,
-                            userBody = UserBody(
-                                userId = loggedUserId,
-                                userName = editUserName.text.toString(),
-                                email = loggedEmail,
-                                password = loggedPassword
-                            )
-                        )
-                    call.enqueue(object:retrofit2.Callback<Void> {
-                        override fun onFailure(call: Call<Void>, t: Throwable) {
-                            println(t.message)
-                            Toast.makeText(
-                                this@EditUserActivity,
-                                t.message,
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-
-                        override fun onResponse(call: Call<Void>, response: Response<Void>) {
-                            println(response.body())
-                            if (response.code() == 200) {
-                                Toast.makeText(this@EditUserActivity, "User is updated", Toast.LENGTH_SHORT)
-                                    .show()
-                                val intent = Intent(this@EditUserActivity, MainActivity::class.java)
-                                intent.putExtra("loggedUserId", loggedUserId)
-                                intent.putExtra("loggedUserName", editUserName.text.toString())
-                                intent.putExtra("loggedEmail", loggedEmail)
-                                intent.putExtra("loggedPassword", loggedPassword)
-                                startActivity(intent)
+                if (editUserName.text.toString().length >= 5) {
+                    if (loggedUserId != null && loggedEmail != null && loggedPassword != null) {
+                        val call =
+                                API.putUser(
+                                        id = loggedUserId,
+                                        userBody = UserBody(
+                                                userId = loggedUserId,
+                                                userName = editUserName.text.toString(),
+                                                email = loggedEmail,
+                                                password = loggedPassword
+                                        )
+                                )
+                        call.enqueue(object:retrofit2.Callback<Void> {
+                            override fun onFailure(call: Call<Void>, t: Throwable) {
+                                println(t.message)
+                                Toast.makeText(
+                                        this@EditUserActivity,
+                                        t.message,
+                                        Toast.LENGTH_SHORT
+                                ).show()
                             }
-                            else{
-                                Toast.makeText(this@EditUserActivity, "Failed!", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
-                        }
 
-                    })
+                            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                                println(response.body())
+                                if (response.code() == 200) {
+                                    Toast.makeText(this@EditUserActivity, "User is updated", Toast.LENGTH_SHORT)
+                                            .show()
+                                    val intent = Intent(this@EditUserActivity, MainActivity::class.java)
+                                    intent.putExtra("loggedUserId", loggedUserId)
+                                    intent.putExtra("loggedUserName", editUserName.text.toString())
+                                    intent.putExtra("loggedEmail", loggedEmail)
+                                    intent.putExtra("loggedPassword", loggedPassword)
+                                    startActivity(intent)
+                                }
+                                else{
+                                    Toast.makeText(this@EditUserActivity, "Failed!", Toast.LENGTH_SHORT)
+                                            .show()
+                                }
+                            }
+
+                        })
+                    }
+                } else {
+                    editUserName.error = "User Name must be 5 or more characters in length"
                 }
             } else {
                 Toast.makeText(this@EditUserActivity, "User Name field is empty", Toast.LENGTH_SHORT)
                     .show()
-                editText.error = "User Name field cannot be empty"
+                editUserName.error = "User Name field cannot be empty"
             }
         }
     }
--- ToDoListAndroidApp/app/src/main/java/com/example/to_dolistandroidapp/RegisterActivity.kt
+++ ToDoListAndroidApp/app/src/main/java/com/example/to_dolistandroidapp/RegisterActivity.kt
@@ -1,7 +1,6 @@
 package com.example.to_dolistandroidapp
 
 import Models.SignUpUser
-import Models.TaskModelItem
 import android.content.Intent
 import kotlinx.android.synthetic.main.activity_register.*
 import androidx.appcompat.app.AppCompatActivity
@@ -22,15 +21,17 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        var rf = Retrofit.Builder()
+        val rf = Retrofit.Builder()
             .baseUrl(RetrofitInterface.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create()).build()
 
-        var API = rf.create(RetrofitInterface::class.java)
+        val API = rf.create(RetrofitInterface::class.java)
 
         btnSignUp.setOnClickListener {
             if (userNameSignUp.text.isEmpty()) {
                 userNameSignUp.error = "Fill User Name field"
+            } else if (userNameSignUp.text.toString().length < 5) {
+                userNameSignUp.error = "User Name must be 5 or more characters in length"
             }
             if (emailSignUp.text.isEmpty()){
                 emailSignUp.error = "Fill Email field"
@@ -39,18 +40,18 @@ class RegisterActivity : AppCompatActivity() {
             }
             if (passwordSignUp.text.isEmpty()) {
                 passwordSignUp.error = "Fill Password field"
-            } else if (!IsValidPassword(passwordSignUp.text.toString())) {
+            } else if (!isValidPassword(passwordSignUp.text.toString())) {
                 passwordSignUp.error = "Password must be 8 or more characters in length and must contain 1 or more digit character"
             }
             if (userNameSignUp.error.isNullOrEmpty() && emailSignUp.error.isNullOrEmpty() && passwordSignUp.error.isNullOrEmpty()) {
-                var encodedPassword = Base64.getEncoder().encodeToString(passwordSignUp.text.toString().toByteArray())
-                var encodedEmail = Base64.getEncoder().encodeToString(emailSignUp.text.toString().toByteArray())
+                val encodedPassword = Base64.getEncoder().encodeToString(passwordSignUp.text.toString().toByteArray())
+                val encodedEmail = Base64.getEncoder().encodeToString(emailSignUp.text.toString().toByteArray())
                 signUp(userNameSignUp.text.toString(), encodedEmail, encodedPassword, API)
             }
         }
     }
 
-    fun signUp(userName: String, email: String, password: String, API: RetrofitInterface) {
+    private fun signUp(userName: String, email: String, password: String, API: RetrofitInterface) {
         val signUpUserInfo = SignUpUser(userName, password, email)
         API.signUpPost(signUpUserInfo).enqueue(object : Callback<SignUpUser> {
             override fun onFailure(call: Call<SignUpUser>, t: Throwable) {
@@ -76,10 +77,10 @@ class RegisterActivity : AppCompatActivity() {
 
         })
     }
-    fun isValidEmail(email: String): Boolean {
+    private fun isValidEmail(email: String): Boolean {
         return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
-    fun  IsValidPassword(password: String): Boolean {
+    private fun  isValidPassword(password: String): Boolean {
         val regex = Regex("(?=.*[0-9]).{8,}$", RegexOption.IGNORE_CASE)
         return regex.matches(password)
     }
